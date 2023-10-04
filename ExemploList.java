0f03844431292b27c85a9f49772ef0a9e5f7ab47
Rian Gomes <80797632+riangomesz@users.noreply.github.com>
2023-10-04 20:46:02 -0300

    Create ExemploList.java

---
+++ ExemploList.java
@@ -0,0 +1,19 @@
+package aulas.colecoes;
+
+import java.util.*;
+
+public class ExemploList {
+    public static void main(String[] args)
+    {
+        // Criando um objeto da classe List
+        // Declarando um objeto do tipo String com
+        // referencia a classe ArrayList
+        List<String> al = new ArrayList<String>();
+
+        al.add("Programação");
+        al.add("Solujções");
+        al.add("Computacionais");
+
+        System.out.println(al);
+    }
+}
